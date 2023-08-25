714772a69e3bdea338b22631a97f62a3eda152b6
RafaOB95 <127167255+RafaOB95@users.noreply.github.com>
2023-08-24 21:52:44 -0600

    Add files via upload

---
+++ heart_base.sql
@@ -0,0 +1,16 @@
+CREATE TABLE heart_table (
+    age smallint NOT NULL,
+    sex smallint NOT NULL,
+	cp smallint NOT NULL,
+	trestbps smallint NOT NULL,
+	chol smallint NOT NULL,
+	fbs smallint NOT NULL,
+	restecg smallint NOT NULL,
+	thalach smallint NOT NULL,
+	exang smallint NOT NULL,
+	oldpeak float NOT NULL,
+	slope smallint NOT NULL,
+	ca smallint NOT NULL,
+	thal smallint NOT NULL,
+	target smallint NOT NULL
+);
